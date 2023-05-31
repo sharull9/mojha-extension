443fb1e1d1b8169de919c992c3b5f79ae5c1790e
sharull9 <sharullmusicplaylist@gmail.com>
2023-05-31 17:50:26 +0530

    Initial commit

---
+++ background.js
@@ -0,0 +1,7 @@
+chrome.tabs.query({ lastFocusedWindow: true, active: true }, function (tabs) {
+  let tabUrl = tabs[0].url.split("/")[2];
+  document.getElementById("siteUrl").innerHTML = tabUrl;
+  document.getElementById("siteLink").setAttribute("href", tabUrl);
+  document.getElementById("next").innerHTML = window.innerWidth;
+  console.log(tabs[0].url);
+});
