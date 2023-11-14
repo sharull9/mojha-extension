3861cdd2c14058748e1608d2e77757a67ee0199f
sharull9 <sharullmusicplaylist@gmail.com>
2023-11-14 19:22:34 +0530

    updated layout

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,6 +3,7 @@ console.log('heelo');
 
 
 let root = document.getElementById('root');
+let tab = document.getElementById('tab');
 
 function addChildToRootElement() {
     let childArray = ['overview', 'suggestion', 'summary', 'meta-spec', 'meta-tag', 'seo-heading', 'seo-description', 'page-quality', 'media-seo', 'link-seo', 'search-preview', 'server-config', 'page-speed']
@@ -11,9 +12,16 @@ function addChildToRootElement() {
         let div = document.createElement("div")
         div.style.height = "50px"
         // div.innerHTML = loader(child)
-        div.innerHTML = tabButton(child)
-        div.id = child
-        root.appendChild(div);
+        // div.innerHTML = tabButton(child)
+        // div.id = child
+        // root.appendChild(div);
+    })
+    childArray.map((child) => {
+        // let div = document.createElement("div")
+        // div.style.height = "50px"
+        // div.innerHTML += tabButton(child)
+        // div.id = child
+        tab.innerHTML += tabButton(child);
     })
 }
 
@@ -27,6 +35,6 @@ addChildToRootElement()
 // }
 
 function tabButton(title) {
-    let item = `<div class="max-w-sm animate-pulse h-10 bg-gray-400 rounded-lg">${title}</div>`
+    let item = `<div class="border h-10 cursor-pointer bg-gray-200 rounded-lg flex items-center p-2">${title}</div>`
     return item
 }
